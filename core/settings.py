5657a5ca45d58c5bb25d59ac22d3b9edb4459eb9
Akbarjon <abdurasuloff2004@gmail.com>
2023-09-24 23:59:20 +0500

    4-dars

--- core/settings.py
+++ core/settings.py
@@ -72,6 +72,20 @@ TEMPLATES = [
 #WSGI_APPLICATION = 'core.wsgi.application'
 ASGI_APPLICATION = 'core.asgi.application'
 
+#CHANNEL_LAYERS = {
+#    "default": {
+#        "BACKEND": "channels_redis.core.RedisChannelLayer",
+#        "CONFIG": {
+#            "hosts": [("127.0.0.1", 6379)],
+#        },
+#    },
+#}
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
